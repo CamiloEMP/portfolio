5cf4a64a6bf9d4650f695ec094c047f03cb6f9d7
b7c8p <morapaezc@gmail.com>
2023-01-17 15:19:02 -0500

    add dictionary technologies, miss some imgs

---
+++ src/constants/technologies.ts
@@ -0,0 +1,44 @@
+export const technologies = {
+  'nextjs': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'tailwind': {
+    img: '/technologies/tailwindcss.svg',
+    name: 'Tailwind'
+  },
+  'typescript': {
+    img: '/technologies/typescript.svg',
+    name: 'Typescript'
+  },
+  'nodejs': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'firebase': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'mongo': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'supabase': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'javascript': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'html': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+  'css': {
+    img: '/technologies/nextjs.svg',
+    name: 'NextJS'
+  },
+}
+
+export type KeysTechnologies = keyof typeof technologies
