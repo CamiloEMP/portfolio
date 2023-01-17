dfb16cc1aeadc3668ae097d524e1070c1bfab922
b7c8p <morapaezc@gmail.com>
2023-01-17 18:36:31 -0500

    add section of skills

--- src/constants/technologies.ts
+++ src/constants/technologies.ts
@@ -1,44 +1,72 @@
-export const technologies = {
+export const technologies: ITechologies = {
+  'react': {
+    img: '/technologies/react.svg',
+    name: 'React',
+    category: 'main'
+  },
   'nextjs': {
     img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    name: 'NextJS',
+    category: 'main'
   },
   'tailwind': {
     img: '/technologies/tailwindcss.svg',
-    name: 'Tailwind'
+    name: 'Tailwind',
+    category: 'main'
+
   },
   'typescript': {
     img: '/technologies/typescript.svg',
-    name: 'Typescript'
+    name: 'Typescript',
+    category: 'main'
+
   },
   'nodejs': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/nodejs.svg',
+    name: 'Node.js',
+    category: 'main'
+
   },
   'firebase': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/firebase.svg',
+    name: 'Firebase',
+    category: 'worked'
   },
   'mongo': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/mongodb.svg',
+    name: 'MongoDB',
+    category: 'worked'
   },
   'supabase': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/supabase.svg',
+    name: 'Supabase',
+    category: 'worked'
   },
   'javascript': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/javascript.svg',
+    name: 'HTML5',
+    category: 'scencials'
   },
   'html': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/html5.svg',
+    name: 'JavaScript',
+    category: 'scencials'
   },
   'css': {
-    img: '/technologies/nextjs.svg',
-    name: 'NextJS'
+    img: '/technologies/css.svg',
+    name: 'CSS3',
+    category: 'scencials'
   },
+}
+
+export interface ITechnology {
+  img: string
+  name: string,
+  category: 'main' | 'worked' | 'scencials'
+}
+
+interface ITechologies {
+  [key: string]: ITechnology
 }
 
 export type KeysTechnologies = keyof typeof technologies
